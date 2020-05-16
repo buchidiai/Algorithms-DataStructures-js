a3c8fc14a80776acd0d7f17e5334adbb752951b1
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-15 17:45:23 -0700

    more solution reverse string

--- excercises/reverseString/index.js
+++ excercises/reverseString/index.js
@@ -7,13 +7,26 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split("").reverse().join("");
+  return str.split("").reduce((rev, char) => char + rev, "");
 }
 
-//solution
+//solution 1
 
 // turn string into an array
 // call reverse method on array
 // join array back to string
+
+//solution 1 ->   return str.split("").reverse().join("");
+
+//solution 2
+
+// use (for of) to loop through character
+// concat each character to string
+
+// solution 2 ->
+//   let newString = "";
+//   for (let char of str) {
+//   return  newString = char + newString;
+//   }
 
 module.exports = reverse;
