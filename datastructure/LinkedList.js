560f8ca06a74d7fb1b6e47708f1e45352f1ddeb5
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-19 13:30:34 -0700

    added prepend method to linked list

--- datastructure/LinkedList.js
+++ datastructure/LinkedList.js
@@ -5,8 +5,8 @@ class LinkedList {
       next: null,
     };
     this.tail = this.head;
+
     this.length = 1;
-    // console.log(this.head);
   }
   append(value) {
     const newNode = {
@@ -14,18 +14,35 @@ class LinkedList {
       next: null,
     };
     //add new value to tail
-    this.tail.next = newNode;
-    this.tail = newNode;
+    this.tail.next = newNode; //null
+    this.tail = newNode; //new value & next is null
     this.length++;
 
-    console.log(this);
+    // console.log(this);
+    return this;
+  }
+
+  prepend(value) {
+    //create node object
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    //set next to existing head
+    newNode.next = this.head;
+    //set head to newNode
+    this.head = newNode;
+    this.length++;
+
     return this;
   }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
-myLinkedList.append(16);
+myLinkedList.append(22);
 myLinkedList.append(19);
 myLinkedList.append(33);
 myLinkedList.append(55);
+myLinkedList.prepend(99);
