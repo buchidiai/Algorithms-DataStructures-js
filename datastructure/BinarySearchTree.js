3b3420059630b102f3e9579da74f71df2606e3da
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-24 13:06:31 -0700

    added preorder inorder postorder for bfs implementation

--- datastructure/BinarySearchTree.js
+++ datastructure/BinarySearchTree.js
@@ -102,6 +102,50 @@ class BinarySearchTree {
 
     return this.BreadthFirstSearchRecursive(queue, list);
   }
+
+  DFTInOrder() {
+    return traverseInOrder(this.root, []);
+  }
+
+  DFTPreOrder() {
+    return traversePreOrder(this.root, []);
+  }
+  DFTPostOrder() {
+    return traversePostOrder(this.root, []);
+  }
+}
+
+function traversePreOrder(node, list) {
+  list.push(node.value);
+  if (node.left) {
+    traversePreOrder(node.left, list);
+  }
+  if (node.right) {
+    traversePreOrder(node.right, list);
+  }
+  return list;
+}
+
+function traverseInOrder(node, list) {
+  if (node.left) {
+    traverseInOrder(node.left, list);
+  }
+  list.push(node.value);
+  if (node.right) {
+    traverseInOrder(node.right, list);
+  }
+  return list;
+}
+
+function traversePostOrder(node, list) {
+  if (node.left) {
+    traversePostOrder(node.left, list);
+  }
+  if (node.right) {
+    traversePostOrder(node.right, list);
+  }
+  list.push(node.value);
+  return list;
 }
 
 const tree = new BinarySearchTree();
